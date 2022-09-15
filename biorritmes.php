802ccc5af6b42e867c515d0e4936d099b264614a
Pau Serra <2001pauserra@gmail.com>
2022-09-15 16:31:25 +0200

    New: Added better visuals.

---
+++ biorritmes.php
@@ -0,0 +1,31 @@
+<?php
+
+    class Biorhythm
+    {
+
+        public function getDate($date) 
+        {
+
+            $today = new DateTime();
+
+            $diff = date_diff($date, $today);
+
+            return $diff->format('%R%a');
+
+        }
+
+        public function getPercent($days, $periode) {
+
+            $cicle = $days / $periode;
+
+            $radiants = ((2 * M_PI) * $cicle);
+
+            $sinus = sin($radiants);
+
+            $percent = ((($sinus + 1) * 100) / 2);
+
+            return number_format($percent, 2);
+
+        }
+
+    }
